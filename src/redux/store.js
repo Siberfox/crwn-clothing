50fd5cd80d073ac1966fc30108cfaa308ecbbe60
Andrew I <greyfoxjob@gmail.com>
2020-09-08 15:13:35 +0600

    added redux devtool extension

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,6 +3,8 @@ import logger from "redux-logger";
 import { persistStore } from "redux-persist";
 import createSagaMiddleware from "redux-saga";
 
+import { composeWithDevTools } from "redux-devtools-extension";
+
 import rootSaga from "./root.saga";
 
 import rootReducer from "./root-reducer";
@@ -15,7 +17,10 @@ if (process.env.NODE_ENV === "development") {
   middlewares.push(logger);
 }
 
-export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+export const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(...middlewares))
+);
 
 sagaMiddleware.run(rootSaga);
 
